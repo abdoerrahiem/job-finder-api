2f8cdad71c9710463da32e95628c66f68486e25d
Abdur Rahim <abdoerrahiem@gmail.com>
2021-08-25 22:31:31 +0700

    edit server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const fs = require('fs')
+// const fs = require('fs')
 // const http = require('http')
 // const { Server } = require('socket.io')
 const { notFoundRoute, errorHandler } = require('./middlewares/errors')
@@ -31,6 +31,5 @@ app.use('/notifications', require('./app/routes/notifications'))
 app.use(notFoundRoute)
 app.use(errorHandler)
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-)
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
